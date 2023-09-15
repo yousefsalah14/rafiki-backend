979b5838d3a61663db994e254dec78b4de883b48
Mazin Islam <93228608+N1ghtHunter@users.noreply.github.com>
2023-09-15 15:06:36 +0300

    Update auth middleware

--- src/middlewares/Auth.js
+++ src/middlewares/Auth.js
@@ -4,41 +4,45 @@
  * @returns {Function} - A middleware function that checks if the user is authorized to access the route
  */
 const checkAuthorization = (roleNames) => (req, res, next) => {
-    const sessionId = req.headers.authorization.split(' ')[1];
-    req.sessionStore.get(sessionId, (err, session) => {
-        if (err) {
-            console.log("session err", err);
-            res.status(500).send({ success: false, message: 'Internal Server Error.' });
-        } else {
-            if (session?.IsLoggedIn && roleNames.includes(session?.RoleName)) {
-                req.body.session = session;
-                req.body.sessionId = sessionId;
-                next();
-            } else {
-                res.status(401).send({ success: false, message: 'Unauthorized.' });
-            }
-        }
-    });
+	if (!req.headers.authorization) {
+		res.status(401).send({ success: false, message: 'Unauthorized.' });
+		return;
+	}
+	const sessionId = req.headers.authorization.split(' ')[1];
+	req.sessionStore.get(sessionId, (err, session) => {
+		if (err) {
+			console.log('session err', err);
+			res.status(500).send({ success: false, message: 'Internal Server Error.' });
+		} else {
+			if (session?.IsLoggedIn && roleNames.includes(session?.RoleName)) {
+				req.body.session = session;
+				req.body.sessionId = sessionId;
+				next();
+			} else {
+				res.status(401).send({ success: false, message: 'Unauthorized.' });
+			}
+		}
+	});
 };
 
 /**
  * Middleware functions for specific roles
  */
-const isAlumni = checkAuthorization(["Alumni"]);
-const isAdmin = checkAuthorization(["Admin"]);
-const isStudent = checkAuthorization(["Student"]);
-const isHR = checkAuthorization(["HR"]);
-const isAuthorized = checkAuthorization(["Alumni", "Admin", "Student", "HR", "Professor"]);
-const isAlumniOrStudent = checkAuthorization(["Alumni", "Student"]);
-const isProfessor = checkAuthorization(["Professor"]);
+const isAlumni = checkAuthorization(['Alumni']);
+const isAdmin = checkAuthorization(['Admin']);
+const isStudent = checkAuthorization(['Student']);
+const isHR = checkAuthorization(['HR']);
+const isAuthorized = checkAuthorization(['Alumni', 'Admin', 'Student', 'HR', 'Professor']);
+const isAlumniOrStudent = checkAuthorization(['Alumni', 'Student']);
+const isProfessor = checkAuthorization(['Professor']);
 
 module.exports = {
-    isAlumni,
-    isAdmin,
-    isStudent,
-    isHR,
-    isAuthorized,
-    isAlumniOrStudent,
-    isProfessor,
-    checkAuthorization
+	isAlumni,
+	isAdmin,
+	isStudent,
+	isHR,
+	isAuthorized,
+	isAlumniOrStudent,
+	isProfessor,
+	checkAuthorization,
 };
