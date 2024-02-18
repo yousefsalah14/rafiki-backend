298fce60c3ddb29b5d5135db737a70d459e1c83c
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-02-19 01:44:24 +0200

    feat(validator-auth): add messages for the errors

--- src/validators/auth.validators.js
+++ src/validators/auth.validators.js
@@ -1,35 +1,59 @@
 const { body, param } = require('express-validator')
-const { handleValidationErrors, strict, password } = require('./_common/base.validators')
+const {
+   handleValidationErrors,
+   strict,
+   password,
+} = require('./_common/base.validators')
 
 exports.register = [
-    body('UserName').isString().isLength({ min: 3, max: 50 }),  
-    password(body('Password')),
-    body('Email').isEmail(),
-    body('Role_Id').isInt(),
-    body('Date_Of_Birth').isDate(),
-    body('FirstName').isString().isLength({ min: 3, max: 50 }),
-    body('LastName').isString().isLength({ min: 3, max: 50 }),
-    
+   body('UserName')
+      .isString()
+      .withMessage('username must be a string')
+      .isLength({ min: 3, max: 50 })
+      .withMessage('username must be 3-50 characters'),
+
+   password(body('Password')),
+
+   body('Email').isEmail().withMessage('invalid email'),
+   body('Role_Id').isInt().withMessage('invalid role id'),
+   body('Date_Of_Birth').isDate().withMessage('invalid date of birth'),
+   body('FirstName')
+      .isString()
+      .withMessage('FirstName must be a string')
+      .isLength({ min: 3, max: 50 })
+      .withMessage('FirstName must be 3-50 characters'),
+   body('LastName')
+      .isString()
+      .withMessage('LastName must be a string')
+      .isLength({ min: 3, max: 50 })
+      .withMessage('LastName must be 3-50 characters'),
+
    handleValidationErrors,
    strict,
 ]
 
 exports.login = [
-    body('UserName').isString().isLength({ min: 3, max: 50 }),
-    body('Password').isString().isLength({ min: 8, max: 50 }),
-    handleValidationErrors,
-    strict,
+   body('UserName')
+      .isString()
+      .withMessage('username must be a string')
+      .isLength({ min: 3, max: 50 }),
+   body('Password')
+      .isString()
+      .isLength({ max: 50 })
+      .withMessage('password must be a string and less than 50 characters'),
+   handleValidationErrors,
+   strict,
 ]
 
 exports.sendResetPasswordEmail = [
-    body('Email').isEmail(),
-    handleValidationErrors,
-    strict,
+   body('Email').isEmail().withMessage('invalid email'),
+   handleValidationErrors,
+   strict,
 ]
 
 exports.changePassword = [
-    param('token').isString(),
-    password(body('Password')),
-    handleValidationErrors,
-    strict,
+   param('token').isString().withMessage('invalid token'),
+   password(body('Password')),
+   handleValidationErrors,
+   strict,
 ]
