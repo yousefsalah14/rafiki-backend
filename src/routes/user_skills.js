57ef698f30a8291d3763383a26216d7dd6ba8060
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-02-19 01:57:46 +0200

    feat(userSkills-validator): add validations to the route userSkills

--- src/routes/user_skills.js
+++ src/routes/user_skills.js
@@ -1,13 +1,14 @@
 const router = require('express').Router();
 const { isAuthorized } = require('../middlewares/Auth');
 const userSkillsController = require('../controllers/userSkillsController');
+const validator = require('../validators/userSkills.validators');
 
 router.get('/', isAuthorized, userSkillsController.getAllUserSkills);
 
-router.post('/', isAuthorized, userSkillsController.createUserSkill);
+router.post('/', isAuthorized, validator.createUserSkill, userSkillsController.createUserSkill);
 
-router.put('/:id', isAuthorized, userSkillsController.updateUserSkill);
+router.put('/:id', isAuthorized, validator.updateUserSkill, userSkillsController.updateUserSkill);
 
-router.delete('/:id', isAuthorized, userSkillsController.deleteUserSkill);
+router.delete('/:id', isAuthorized, validator.deleteUserSkill, userSkillsController.deleteUserSkill);
 
 module.exports = router;
