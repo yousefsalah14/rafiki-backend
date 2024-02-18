cf27cd24ed06366566a3dca90af95da38014628d
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-02-19 01:50:51 +0200

    feat(skills-validator) updateSkill validator

--- src/validators/skills.validators.js
+++ src/validators/skills.validators.js
@@ -10,3 +10,14 @@ exports.addSkill = [
    handleValidationErrors,
    strict,
 ]
+
+exports.updateSkill = [
+   param('id').isInt().withMessage('invalid skill id'),
+   body('Skill_Name')
+      .isString()
+      .withMessage('Skill Name must be a string')
+      .isLength({ min: 3, max: 50 })
+      .withMessage('Skill Name must be 3-50 characters'),
+   handleValidationErrors,
+   strict,
+]
