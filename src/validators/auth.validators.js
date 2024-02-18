63dffa3c0ed7db46804e17fba2672512293a61b7
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-02-19 01:37:44 +0200

    feat(finalize-auth-validator)

--- src/validators/auth.validators.js
+++ src/validators/auth.validators.js
@@ -1,19 +1,15 @@
 const { body, param } = require('express-validator')
 const { handleValidationErrors, strict, password } = require('./_common/base.validators')
 
-exports.getAllSessions = [strict]
-
-
 exports.register = [
     body('UserName').isString().isLength({ min: 3, max: 50 }),  
-    body('Password').isString().isLength({ min: 8, max: 50 }),
+    password(body('Password')),
     body('Email').isEmail(),
     body('Role_Id').isInt(),
     body('Date_Of_Birth').isDate(),
     body('FirstName').isString().isLength({ min: 3, max: 50 }),
     body('LastName').isString().isLength({ min: 3, max: 50 }),
     
-
    handleValidationErrors,
    strict,
 ]
