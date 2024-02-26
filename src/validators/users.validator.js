09a0a74755b431bcdd93bfe45924c063e45698d1
rehabibrahim60 <132395261+rehabibrahim60@users.noreply.github.com>
2024-02-27 01:28:38 +0200

    fix(user-validator): add messages for national id

--- src/validators/users.validator.js
+++ src/validators/users.validator.js
@@ -11,7 +11,11 @@ exports.addAlumni = [
         .isLength({ min: 3, max: 50 }).withMessage('UserName must be 3-50 characters'),
     password(body('Password')),
     body('Email').isEmail().withMessage('invalid email'),
-    body('National_Id').isInt().matches('^(2|3)[0-9][0-9][0-1][0-9][0-3][0-9](01|02|03|04|11|12|13|14|15|16|17|18|19|21|22|23|24|25|26|27|28|29|31|32|33|34|35|88)\d\d\d\d\d$'),
+    body('National_Id')
+        .isInt()
+        .withMessage('National Id must be an integer')
+        .matches('^(2|3)[0-9][0-9][0-1][0-9][0-3][0-9](01|02|03|04|11|12|13|14|15|16|17|18|19|21|22|23|24|25|26|27|28|29|31|32|33|34|35|88)\d\d\d\d\d$')
+        .withMessage('invalid input'),
     handleValidationErrors,
     strict
 ]
