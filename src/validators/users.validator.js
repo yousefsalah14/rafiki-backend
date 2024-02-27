1d672993024c2407e42643f60756574f83842a06
rehabibrahim60 <132395261+rehabibrahim60@users.noreply.github.com>
2024-02-27 02:15:57 +0200

    feat(user-validator):add validation for checkNationalIdExists

--- src/validators/users.validator.js
+++ src/validators/users.validator.js
@@ -61,3 +61,13 @@ exports.addProfessor = [
     handleValidationErrors,
     strict
 ]
+
+exports.checkNationalIdExists = [
+    body('NID')
+        .isInt()
+        .withMessage('National Id must be an integer')
+        .matches('^(2|3)[0-9][0-9][0-1][0-9][0-3][0-9](01|02|03|04|11|12|13|14|15|16|17|18|19|21|22|23|24|25|26|27|28|29|31|32|33|34|35|88)\d\d\d\d\d$')
+        .withMessage('invalid input'),
+    handleValidationErrors,
+    strict
+]
