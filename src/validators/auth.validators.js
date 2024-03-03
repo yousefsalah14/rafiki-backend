b64b2711c1fbc8e8927a59cf97f95a1bba638575
Mazin Islam <mazinislam431@gmail.com>
2024-03-03 14:56:20 +0200

    fix: use express-validator for email format check

--- src/validators/auth.validators.js
+++ src/validators/auth.validators.js
@@ -1,59 +1,69 @@
 const { body, param } = require('express-validator');
 const {
-  handleValidationErrors,
-  strict,
-  password,
+    handleValidationErrors,
+    strict,
+    password,
 } = require('./_common/base.validators');
 
 exports.register = [
-  body('UserName')
-    .isString()
-    .withMessage('username must be a string')
-    .isLength({ min: 3, max: 50 })
-    .withMessage('username must be 3-50 characters'),
+    body('UserName')
+        .isString()
+        .withMessage('username must be a string')
+        .isLength({ min: 3, max: 50 })
+        .withMessage('username must be 3-50 characters'),
 
-  password(body('Password')),
+    password(body('Password')),
 
-  body('Email').isEmail().withMessage('invalid email'),
-  body('Role_Id').isInt().withMessage('invalid role id'),
-  body('Date_Of_Birth').isDate().withMessage('invalid date of birth'),
-  body('FirstName')
-    .isString()
-    .withMessage('FirstName must be a string')
-    .isLength({ min: 3, max: 50 })
-    .withMessage('FirstName must be 3-50 characters'),
-  body('LastName')
-    .isString()
-    .withMessage('LastName must be a string')
-    .isLength({ min: 3, max: 50 })
-    .withMessage('LastName must be 3-50 characters'),
+    body('Email')
+        .isEmail()
+        .withMessage('Must be a valid email')
+        .custom((value) => {
+            const regexp = /\b[A-Za-z0-9._%+-]+@fci\.helwan\.edu\.eg\b/;
+            if (!regexp.test(value)) {
+                throw new Error('This email doesn’t meet the required format');
+            }
+            return true;
+        }),
+    ,
+    body('Role_Id').isInt().withMessage('invalid role id'),
+    body('Date_Of_Birth').isDate().withMessage('invalid date of birth'),
+    body('FirstName')
+        .isString()
+        .withMessage('FirstName must be a string')
+        .isLength({ min: 3, max: 50 })
+        .withMessage('FirstName must be 3-50 characters'),
+    body('LastName')
+        .isString()
+        .withMessage('LastName must be a string')
+        .isLength({ min: 3, max: 50 })
+        .withMessage('LastName must be 3-50 characters'),
 
-  handleValidationErrors,
-  strict,
+    handleValidationErrors,
+    strict,
 ];
 
 exports.login = [
-  body('UserName')
-    .isString()
-    .withMessage('username must be a string')
-    .isLength({ min: 3, max: 50 }),
-  body('Password')
-    .isString()
-    .isLength({ max: 50 })
-    .withMessage('password must be a string and less than 50 characters'),
-  handleValidationErrors,
-  strict,
+    body('UserName')
+        .isString()
+        .withMessage('username must be a string')
+        .isLength({ min: 3, max: 50 }),
+    body('Password')
+        .isString()
+        .isLength({ max: 50 })
+        .withMessage('password must be a string and less than 50 characters'),
+    handleValidationErrors,
+    strict,
 ];
 
 exports.sendResetPasswordEmail = [
-  body('email').isEmail().withMessage('invalid email'),
-  handleValidationErrors,
-  strict,
+    body('email').isEmail().withMessage('invalid email'),
+    handleValidationErrors,
+    strict,
 ];
 
 exports.changePassword = [
-  param('token').isString().withMessage('invalid token'),
-  password(body('password')),
-  handleValidationErrors,
-  strict,
+    param('token').isString().withMessage('invalid token'),
+    password(body('password')),
+    handleValidationErrors,
+    strict,
 ];
